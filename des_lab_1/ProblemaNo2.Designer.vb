6109543ca1ed5fd527fcba574c88f873678c301d
Ever_Green <evergreen1504@gmail.com>
2021-09-04 18:42:29 -0500

    Problema No 2 del Laboratorio No1

---
+++ des_lab_1/ProblemaNo2.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ProblemaNo2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ProblemaNo2))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtEdaddejuan = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtJuan = New System.Windows.Forms.TextBox()
+        Me.txtAlberto = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtAna = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtMdeGisela = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 22)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enunciado: "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(138, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(575, 60)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = resources.GetString("Label2.Text")
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(282, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(210, 19)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "¿Cuál es la edad de Juan?"
+        '
+        'txtEdaddejuan
+        '
+        Me.txtEdaddejuan.Location = New System.Drawing.Point(357, 139)
+        Me.txtEdaddejuan.Name = "txtEdaddejuan"
+        Me.txtEdaddejuan.Size = New System.Drawing.Size(66, 20)
+        Me.txtEdaddejuan.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(104, 345)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Juan"
+        '
+        'txtJuan
+        '
+        Me.txtJuan.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtJuan.Location = New System.Drawing.Point(107, 368)
+        Me.txtJuan.Name = "txtJuan"
+        Me.txtJuan.ReadOnly = True
+        Me.txtJuan.Size = New System.Drawing.Size(34, 29)
+        Me.txtJuan.TabIndex = 5
+        Me.txtJuan.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtAlberto
+        '
+        Me.txtAlberto.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAlberto.Location = New System.Drawing.Point(293, 368)
+        Me.txtAlberto.Name = "txtAlberto"
+        Me.txtAlberto.ReadOnly = True
+        Me.txtAlberto.Size = New System.Drawing.Size(34, 29)
+        Me.txtAlberto.TabIndex = 7
+        Me.txtAlberto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(289, 345)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 20)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Alberto"
+        '
+        'txtAna
+        '
+        Me.txtAna.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAna.Location = New System.Drawing.Point(465, 368)
+        Me.txtAna.Name = "txtAna"
+        Me.txtAna.ReadOnly = True
+        Me.txtAna.Size = New System.Drawing.Size(34, 29)
+        Me.txtAna.TabIndex = 9
+        Me.txtAna.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(467, 345)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(32, 20)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Ana"
+        '
+        'txtMdeGisela
+        '
+        Me.txtMdeGisela.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMdeGisela.Location = New System.Drawing.Point(636, 368)
+        Me.txtMdeGisela.Name = "txtMdeGisela"
+        Me.txtMdeGisela.ReadOnly = True
+        Me.txtMdeGisela.Size = New System.Drawing.Size(34, 29)
+        Me.txtMdeGisela.TabIndex = 11
+        Me.txtMdeGisela.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(605, 345)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(107, 20)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Mamá de Gisela"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(342, 217)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 24)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Calcular Edades"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProblemaNo2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtMdeGisela)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtAna)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtAlberto)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtJuan)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtEdaddejuan)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ProblemaNo2"
+        Me.Text = "ProblemaNo2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtEdaddejuan As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtJuan As TextBox
+    Friend WithEvents txtAlberto As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtAna As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtMdeGisela As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ des_lab_1/ProblemaNo2.vb
@@ -0,0 +1,20 @@
+﻿Public Class ProblemaNo2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim edejuan, edealberto, edeana, edegisela As Integer
+
+        edejuan = Val(txtEdaddejuan.Text)
+
+        If edejuan >= 0 Then
+            edealberto = CInt(edejuan * (2 / 3))
+            edeana = CInt(edejuan * (4 / 3))
+            edegisela = edejuan + edeana + edealberto
+            txtAlberto.Text = edealberto
+            txtAna.Text = edeana
+            txtMdeGisela.Text = edegisela
+            txtJuan.Text = edejuan
+        Else
+            MsgBox("Debe ser un Número positivo")
+            txtEdaddejuan.Clear()
+        End If
+    End Sub
+End Class
