e63073e47999fdb631ddaff123fa5148040c7661
Jawline0700 <wencerscastillo40@gmail.com>
2021-09-04 20:29:33 -0500

    Programa5

--- des_lab_1/Form1.vb
+++ des_lab_1/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnmayor.Click
+        programa5.Show()
 
+    End Sub
 End Class
---
+++ des_lab_1/logica.vb
@@ -0,0 +1,25 @@
+﻿Module logica
+    Public Function Mayor1(ByVal subasta() As Double) As Double
+        Dim contador As Integer
+        Dim mayor = subasta(0)
+
+        For i = 0 To subasta.Length - 1
+            If (subasta(i) > mayor) Then
+                mayor = subasta(i)
+            End If
+        Next
+        Mayor1 = mayor
+    End Function
+
+    Public Function menor(ByVal subasta() As Double) As Double
+        Dim contador As Integer
+        Dim pequeño = subasta(0)
+        For i = 0 To subasta.Length - 1
+            If (subasta(i) <= pequeño) Then
+                pequeño = subasta(i)
+            End If
+        Next
+        menor = pequeño
+    End Function
+
+End Module
